03efe1a08e86390a68dd13246f7b98087e1e8dac
It21165498 <It21165498@my.sliit.lk>
2023-07-22 17:58:46 +0530

    login and registration components added in auth.js

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return (
+    <div className="auth">
+      <Login />
+      <Register />
+    </div>
+  );
 }
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+
+      console.log(response);
+
+      alert(response.data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Register"
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        username,
+        password,
+      });
+
+      if (response.data.token) {
+        setCookies("access_token", response.data.token);
+        window.localStorage.setItem("userID", response.data.userID);
+        navigate("/");
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Login"
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+const Form = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  label,
+  onSubmit,
+}) => {
+  return (
+    <div className="auth-container">
+      <form onSubmit={onSubmit}>
+        <h2> {label} </h2>
+
+        <div className="form-group">
+          <label htmlFor="username"> Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password"> Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+
+        <button type="submit"> {label} </button>
+      </form>
+    </div>
+  );
+};
