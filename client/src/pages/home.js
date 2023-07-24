a332e09ab5e674f8cf09f4166b9983a9413a75ee
It21165498 <It21165498@my.sliit.lk>
2023-07-24 20:01:41 +0530

    save recipe added

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID.js";
 
 export default function Home() {
   const [recipes, setRecipes] = useState([]);
+  const userId = useGetUserID();
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const response = await axios.get("http://localhost:3001/recipes");
         setRecipes(response.data);
-        console.log(response.data);
       } catch (error) {
         console.error(error);
       }
@@ -17,6 +18,18 @@ export default function Home() {
 
     fetchRecipes();
   }, []);
+
+  const saveRecipe = async (recipeId) => {
+    try {
+      const response = await axios.put("http://localhost:3001/recipes", {
+        userId,
+        recipeId,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
       <h1> Recipes </h1>
@@ -26,6 +39,7 @@ export default function Home() {
           <li key={recipe._id}>
             <div>
               <h2> {recipe.name} </h2>
+              <button onClick={() => saveRecipe(recipe._id)}> Save </button>
             </div>
             <div className="instructions">
               <p> {recipe.instructions} </p>
