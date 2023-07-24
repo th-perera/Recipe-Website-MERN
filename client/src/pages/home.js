35cf13777849a6bfeea16a9690b2fc75bd39c7df
It21165498 <It21165498@my.sliit.lk>
2023-07-24 18:51:34 +0530

    get all recipes to homepage added

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  return <div> Home</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+  return (
+    <div>
+      <h1> Recipes </h1>
+
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2> {recipe.name} </h2>
+            </div>
+            <div className="instructions">
+              <p> {recipe.instructions} </p>
+            </div>
+            <img src={recipe.imgUrl} alt={recipe.name} />
+            <p> Cooking Time : {recipe.cookingTime} (minutes) </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
